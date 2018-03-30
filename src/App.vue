56d2745f4c96d58a82a33b6b915ca18bb9699b23
Jull Weber <jull@monkeykodeagency.com>
2018-03-29 22:18:35 -0700

    add files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <AutoComplete></AutoComplete>
+    </div>
+</template>
+
+<script>
+  import AutoComplete from './components/Ac.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      AutoComplete,
+    },
+  }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
